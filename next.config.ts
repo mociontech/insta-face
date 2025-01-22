a8b7ad89a73db14d6c931a19015005b4f349d69c
Juan Sebastian Rodriguez <chechomens@gmail.com>
2025-01-22 10:06:25 -0500

    fix: changing config to next

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig: import('next').NextConfig = {
-  reactStrictMode: false,
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
