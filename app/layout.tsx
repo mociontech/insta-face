4b2e944dcc46e31419c0e01676dc0959fe2b2653
Rosyec <chechomens@gmail.com>
2025-04-22 18:00:10 -0500

    feat: adding manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,15 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Face swap",
+  title: "Claro Gaming",
   description: "Face swap webapp",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <link rel="manifest" href="/manifest.json" />
       <body>{children}</body>
     </html>
   );
