309518c0f62b01410109f96f743bf4c9d153a592
ingcarlosguerra <tech@mocion.com.co>
2025-07-04 12:38:13 -0500

    cambio de API
    se implemento nueva api la cual usa dos POST para que la image

--- lib/faceSwap.ts
+++ lib/faceSwap.ts
@@ -1,28 +1,54 @@
 import axios from "axios";
+import { string } from "yup";
 
 export async function faceSwap(userPhotoUrl, selectedImage) {
   try {
+    const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
     const targetPhotoUrl = `https://storage.googleapis.com/f1-sap.appspot.com/claro/${selectedImage}.png`;
-
+    let apiID = "";
     const options = {
       method: "POST",
-      url: "https://faceswap-image-transformation-api.p.rapidapi.com/faceswapgroup",
+      url: "https://faceswap-api.p.rapidapi.com/faceswap-image",
       headers: {
         "x-rapidapi-key": "3fe4672104mshbf231cb22b48ee9p115b90jsn26c8b41ee7e9",
-        "x-rapidapi-host": "faceswap-image-transformation-api.p.rapidapi.com",
+        "x-rapidapi-host": "faceswap-api.p.rapidapi.com",
         "Content-Type": "application/json",
       },
-      data: {
-        TargetImageUrl: targetPhotoUrl,
-        SourceImageUrl: userPhotoUrl,
-        MatchGender: true,
-        MaximumFaceSwapNumber: 8,
+      data: { "input" :{
+        target_image: targetPhotoUrl,
+        swap_image: userPhotoUrl,
+      }
       },
     };
 
-    const faceSwapRequest = await axios.request(options);
+    const idApi = await axios.request(options);
+    console.log(idApi.data.request_id);
 
-    return faceSwapRequest.data.ResultImageUrl;
+      const options2 = {
+        method: "POST",
+        url: "https://faceswap-api.p.rapidapi.com/result",
+        headers: {
+          "x-rapidapi-key": "3fe4672104mshbf231cb22b48ee9p115b90jsn26c8b41ee7e9",
+          "x-rapidapi-host": "faceswap-api.p.rapidapi.com",
+          "Content-Type": "application/json",
+        },
+        data: {
+          request_id: String(idApi.data.request_id),
+        },
+      };
+
+    await sleep(1000);
+    while(true){
+    try {
+      const faceSwapRequest = await axios.request(options2);
+      if (faceSwapRequest.data.status === 'processed' ){
+        return faceSwapRequest.data.output;
+      } 
+    } catch (err) {
+      console.log(`Try  ${err}`)
+    }
+    }
+
   } catch (error) {
     console.log("Error durante el proceso de face swap:", error);
     return null;
